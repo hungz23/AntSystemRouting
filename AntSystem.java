5171244352fa9b9fa8b1c37ffd863dcba1164f38
hungz23 <hungtranbuddha@gmail.com>
2017-06-07 20:56:38 +0800

    Stupid....

--- AntSystem.java
+++ AntSystem.java
@@ -24,7 +24,7 @@ public class AntSystem {
 		private List<String> bestpath;
 		private double bestcost;
 
-		private final double alpha=2.0;
+		private final double alpha=10.0;
 		private final double beta=3.0;
 
 		private String deltaname;
@@ -38,19 +38,18 @@ public class AntSystem {
 			this.path = new ArrayList<String>();
 			this.bestpath = new ArrayList<String>();
 			this.bestcost = maxcost;
+			this.cost = 0;
 			this.deltaname = "delta"+Integer.toString(antID);
 			this.edgePath = new ArrayList<Edge>();
 			currNode.setAttribute("visited");
 			path.add(currNode.toString());
-			addDeltaToGraph(graph);
 		}
 // Them delta cua kien cho graph
 		private void addDeltaToGraph(Graph graph){
-			for(Edge edge: graph.getEachEdge()){
-				if(!edge.hasAttribute(deltaname)){
-					if(cost==0) edge.addAttribute(deltaname,0.0);
-					else edge.setAttribute(deltaname, 1.0/getcost());
-				}
+			for(String name: bestpath){
+				Edge edge = graph.getEdge(name);
+				double delta = edge.getAttribute(deltaname);
+				edge.setAttribute(deltaname, delta+1/getbestcost());
 			}
 		}
 
@@ -84,9 +83,11 @@ public class AntSystem {
 
 		public void clear(Graph graph){
 			this.currNode = startVertex;
-			for(String id: path){
-				Node node = graph.getNode(id);
+			for(Node node: graph){
 				node.removeAttribute("visited");
+			}
+			for(Edge edge: graph.getEachEdge()){
+				edge.removeAttribute("visited");
 			}
 			this.path = new ArrayList<String>();
 			path.add(currNode.toString());
@@ -127,25 +128,24 @@ public class AntSystem {
 				edge.setAttribute(deltaname, 1/cost);//Cap nhat delta cua kien
 		}
 
-		private boolean halt(Graph graph, Edge edge){//Quyet dinh xem co di ko theo xac suat dc tinh
+		private boolean halt(Graph graph, Edge edge, Node node){//Quyet dinh xem co di ko theo xac suat dc tinh
 			Random random = new Random();
 			double  n = random.nextDouble();
-			if(n < caculate_propability(edge, graph)){
+			if(n < caculate_propability(edge, node, graph)){
 				return true;
 			}
 			return false;
 		}
 
-		private double caculate_propability(Edge edge, Graph graph){//Tinh xac suat theo cong thuc o paper
+		private double caculate_propability(Edge edge, Node node, Graph graph){//Tinh xac suat theo cong thuc o paper
 			double thetaAbove = edge.getAttribute("theta");
 			double num1Above = Math.pow(thetaAbove, alpha);
 			double nuyAbove = edge.getAttribute("nuy");
 			double num2Above = Math.pow(nuyAbove, beta);
 			double numberAbove = num1Above*num2Above;
 			double sum = 0.0;
-			for(Edge e: graph.getEachEdge()){
-				if(!e.hasAttribute("caculate_propability")){
-					e.addAttribute("caculate_propability");
+			for(Edge e: node.getEachEdge()){
+				if(edge.hasAttribute("visited")){
 					double theta = e.getAttribute("theta");
 					double num1 = Math.pow(theta, alpha);
 					double nuy = e.getAttribute("nuy");
@@ -153,54 +153,69 @@ public class AntSystem {
 					double number = num1*num2;
 					sum+=number;
 				}
+				//System.out.println("Nuiberrrrrrrrrrrrrrr::::::"+number);
+
 			}
 			if(sum==0){
-				return 1.0;
+
+				return 0.1;
 			}else{
+				//System.out.println("Above::::::"+numberAbove);
+				//System.out.println("Sum::::::"+sum);
+
+				//System.out.println("Prob::::::"+numberAbove/sum);
 				return numberAbove/sum;
 			}
 		}
 
+		public void test(Graph graph){
+			Node currnode = graph.getNode(0);
+			double n=0;
+			for(Edge edge: currNode.getEachEdge()){
+					n+=caculate_propability(edge, currNode, graph);
+			}
+			System.out.println(n);
+		}
 		public boolean move(Graph graph){
 			Node previous_node = currNode; int count=0;
 			Random rand = new Random();
-			while(currNode == previous_node){
-				int r1 = rand.nextInt(10);
-				if(r1<5){//Xac suat 50% di bua
+				while(currNode==previous_node){
+					double n=0.0;
+					double random = rand.nextDouble();
 					for(Edge edge: currNode.getEachEdge()){
-						int r2 = rand.nextInt(50);
-						if(r2<10){
-							walk(currNode, edge);
+						if(!edge.getOpposite(currNode).hasAttribute("visited")){						
+							if(random>=n && random<=n+caculate_propability(edge, currNode, graph)){
+								walk(currNode, edge);
+				//				System.out.println("Prob:::"+caculate_propability(edge, currNode, graph)+"n::"+n);
+								return true;
+							}
+						}
+						n=n+caculate_propability(edge, currNode, graph);
+
+						++count;
+						if(count > 1000){//Lap qua nhieu ma khong duoc thi di duong khac, do thi to thi cho to ra
+							//Vidu count>1000
+							clear(graph);//Cac thong so tro lai luc dau
 							break;
 						}
 					}
-				}
-				for(Edge edge: currNode.getEachEdge()){
-					if(!edge.getOpposite(currNode).hasAttribute("visited")){						
-						if(halt(graph, edge)){//Neu ham halt tra ve true thi di qua canh nay
-							walk(currNode, edge);
-							break;
-						}
-					}
-					++count;
-					if(count > 100){//Lap qua nhieu ma khong duoc thi di duong khac, do thi to thi cho to ra
-						//Vidu count>1000
-						clear(graph);//Cac thong so tro lai luc dau
-						break;
-					}
-				}
-				if(count==0) return false;  
+						//System.out.println("=============="+n);
+
 			}
+				if(count==0) return false;  
 			
 			return true;
 		}
 //lap lien tuc cho den khi den diem cuoi (endIDVertex)
 		public void iterate(Graph graph){
+			int count=0;
 			while(currNode!=endIDVertex){
-				addDeltaToGraph(graph);//Them delta
 				move(graph);//Kien di chuyen trong graph
-				addPhormone(graph, 1);
+				//System.out.println(currNode.toString()+"--------------");
+				count++;
 			}
+			addDeltaToGraph(graph);//Them delta
+			addPhormone(graph, 1);
 			if(cost<bestcost){
 				bestcost = cost;
 				bestpath = path;
@@ -212,15 +227,17 @@ public class AntSystem {
 		for(Edge edge: graph.getEachEdge()){
 			double theta = edge.getAttribute("theta");
 			double sum = calculateDeltaSum(edge, numberOfAnt);
-			edge.setAttribute("theta", theta+(1-zo)*sum);
+			edge.setAttribute("theta", (theta+sum)*(1-zo));
 		}
 	}
 //tinh tong cac delta
 	public static double calculateDeltaSum(Edge edge, int numberOfAnt){
 		double sum=0;
 		for(int i=0; i<numberOfAnt;i++){
-			double num = edge.getAttribute("delta"+Integer.toString(i));
-			sum+=num;
+			if(edge.hasAttribute("delta"+Integer.toString(i))){
+				double num = edge.getAttribute("delta"+Integer.toString(i));
+				sum+=num;
+			}
 		}
 		return sum;
 	}
@@ -228,7 +245,9 @@ public class AntSystem {
 	public static void makeGraph(Graph graph){
 		// Add weight for edges 
 		for(Edge edge: graph.getEachEdge()){
-			edge.setAttribute("weight", 1.0);
+			Random rand = new Random();
+			double x = rand.nextDouble()+10;
+			edge.setAttribute("weight", x);
 		}
 		
 		//Add nuy for edges
@@ -239,11 +258,14 @@ public class AntSystem {
 
 		//Add theta for edges
 		for(Edge edge: graph.getEachEdge()){
-			edge.setAttribute("theta", 0.0);
+			edge.setAttribute("theta", .1);
 		}	
 	}
 
 	public static void main(String args[]) {
+		List<String> bestpath = new ArrayList<String>();
+		double bestcost=10000;
+
 		Graph graph = new SingleGraph("Random");
 		    Generator gen = new RandomGenerator(50);
 		    gen.addSink(graph);
@@ -260,12 +282,16 @@ public class AntSystem {
 		Node a = graph.getNode(0);
 		Node b = graph.getNode(1);
 		
-		Ant ant0 = new Ant(graph, 0, "ant0", a, b);
 
-		for(int i=0; i<20; i++){
+		for(int i=0; i<450; i++){
+			Ant ant0 = new Ant(graph, 0, "ant0", a, b);
 			ant0.iterate(graph);
+			ant0.test(graph);
+			addPhormone(graph, 1);
+			ant0.clear(graph);
+
 			//dynamicity co the bo de test graph tinh
-			double num = rand.nextDouble();
+			/*double num = rand.nextDouble();
 			for(Edge edge: graph.getEachEdge()){
 				int change = rand.nextInt(200);
 				if(change==0){
@@ -280,13 +306,17 @@ public class AntSystem {
 				double weight = edge.getAttribute("weight");
 				edge.setAttribute("nuy",1.0/weight);
 				//edge.setAttribute("delta0", 0.0);
-			}
+			}*/
 			//dynamicity
 
 			Dijkstra dijkstra = new Dijkstra(Dijkstra.Element.EDGE, null, "weight");
 			dijkstra.init(graph);
 			dijkstra.setSource(a);
 			dijkstra.compute();
+			if(bestcost>ant0.getbestcost()){
+				bestpath = ant0.getbestpath();
+				bestcost = ant0.getbestcost();
+			}
 			System.out.println("dijkstra:::::"+dijkstra.getPath(b));
 			System.out.printf("dijkstra::::::: %s->%s:%10.2f%n", dijkstra.getSource(), b,
 								dijkstra.getPathLength(b));
@@ -295,8 +325,10 @@ public class AntSystem {
 			String.valueOf(ant0.getbestcost()));
 			ant0.clear(graph);
 			System.out.println("================================================");
+			System.out.println("================edge");			
 		}
-		
+		System.out.println("bestpath:::"+bestpath);
+		System.out.println("bestcost:::"+bestcost);
 	}
 }
 //
